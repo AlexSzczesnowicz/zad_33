e5d4ce86a8128f6341bcf6068fc9ca641c3afbd0
AlexSzczesnowicz <193945w@gmail.com>
2020-10-15 21:00:14 +0200

    now works

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-let j = document.getElementById("opis");
-//let k = document.querySelector("button"); nie działa
-let k = document.getElementById("b1");
+let paragraf = document.getElementById('opis');
+let buton = document.querySelector('button');
+
 function opis (x){
-    x.textContent("dowolny text");
+    x.textContent = 'dowolny text';
 }
-k.onclick = opis(j);
+buton.onclick = function() { opis(paragraf) };
+
+/*document.querySelector('button').onclick = function() {
+    paragraf.textContent = 'dowolny text';
+   
+}
+*/
